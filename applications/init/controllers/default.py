dcfd51338e8e200d40627089082a1676639e5a16
Arturo Filastò <art@baculo.org>
2011-08-28 15:17:28 +0200

    refactor code * move all to tulip.py controller * add the tulip/* views

--- applications/init/controllers/default.py
+++ applications/init/controllers/default.py
@@ -41,50 +41,5 @@ def submission():
     
     return dict(form=form, leak_id=None, tulip=None, tulips=None)
 
-def tulip():
-    tulip_url = request.args[0]
-    t = Tulip(url=tulip_url)
-    leak = t.get_leak()
-    dead = False
-    
-    form = SQLFORM.factory(Field('Comment', 'text', requires=IS_NOT_EMPTY()))
-
-    if(int(t.downloads_counter) >= int(t.allowed_downloads) and int(t.allowed_downloads)!=0):
-        dead = True
-    else:
-        t.downloads_counter = int(t.downloads_counter) + 1
-
-    if form.accepts(request.vars, session):
-        response.flash = 'ok!'
-        c = response.vars
-        print response.vars
-
-        return dict(dead=dead,
-                leak_title=leak.title,
-                leak_tags=leak.tags,
-                leak_desc=leak.desc,
-                leak_material=leak.material,
-                tulip_downloads=t.downloads_counter,
-                tulip_allowed_downloads=t.allowed_downloads,
-                comment="asdads", 
-                name=t.target, 
-                comment_form=form)
-
-    elif form.errors:
-        response.flash = 'form has errors'
-    else:
-        response.flash = 'please fill the form'    
-
-
-    return dict(dead=dead,
-                leak_title=leak.title,
-                leak_tags=leak.tags,
-                leak_desc=leak.desc,
-                leak_material=leak.material,
-                tulip_downloads=t.downloads_counter,
-                tulip_allowed_downloads=t.allowed_downloads,
-                comment_form=form,
-                comment=None)
-
 def error():
     return dict()
