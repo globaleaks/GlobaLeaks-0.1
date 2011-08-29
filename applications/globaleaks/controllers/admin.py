9acc9cc865af7d8248ebb87be233677f5440e212
Arturo Filastò <art@baculo.org>
2011-08-29 12:34:10 +0200

    * Add the separate admin controller * Add the views for admin and targets * Add the images for the demo * Add the target list cleaning cron job

---
+++ applications/globaleaks/controllers/admin.py
@@ -0,0 +1,36 @@
+# coding: utf8
+# try something like
+
+@auth.requires_login()
+def index(): 
+    return dict(message="hello from admin.py")
+
+@auth.requires_login()
+def targets():
+    
+    response.flash = "You are now the GlobaLeaks Node Maintainer"
+    
+    
+    if(request.vars.edit and request.vars.edit.startswith("delete")):
+        gl.delete_target(request.vars.edit.split(".")[1])
+
+    if(request.vars.edit and request.vars.edit.startswith("edit")):
+        pass
+
+    form_content = (Field('Name', requires=IS_NOT_EMPTY()),
+                    Field('Description', 'text', requires=IS_LENGTH(minsize=5,maxsize=50)),
+                    Field('email', requires=IS_EMAIL())
+                   )
+   
+    form = SQLFORM.factory(*form_content)
+
+    targets = gl.get_targets("ANY")
+    
+    if form.accepts(request.vars, session):
+        c = request.vars
+        gl.create_target(c.Name, "demo", c.Description, c.email, "demo", "demo target")
+        targets = gl.get_targets("ANY")
+
+        return dict(form=form, added=True, targets=targets)
+ 
+    return dict(form=form, added=False, targets=targets)
