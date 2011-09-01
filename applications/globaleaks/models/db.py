f11195d6127c66449b915c27d84ccf63ac67a91d
Seif Lotfy <seif@lotfy.com>
2011-09-01 19:26:25 +0200

    "started exporting globaleaks logic specific stuff into modules directory"

--- applications/globaleaks/models/db.py
+++ applications/globaleaks/models/db.py
@@ -20,70 +20,7 @@ else:                                         # else use a normal relational dat
     db = DAL('sqlite://storage.sqlite')       # if not, use SQLite or other DB
 """
 randomizer = local_import('randomizer')
-
-class DB(DAL):
-    def __init__(self):
-        DAL.__init__(self, 'sqlite://storage.db')
-        self.create_db()
-    
-    def create_db(self):
-        self.define_table('target',
-            Field('name'),
-            Field('category'),
-            Field('desc'),
-            Field('url'),
-            Field('type'),
-            Field('info'),
-            Field('status'),
-            Field('last_sent_tulip'),
-            Field('last_access'),
-            Field('last_download'),
-            Field('tulip_counter'),
-            Field('dowload_counter'),
-            format='%(name)s'
-        )
-            
-        self.define_table('leak',
-            Field('title'),
-            Field('desc'),
-            Field('submission_timestamp'),
-            Field('leaker_id', self.target),
-            Field('spooled', 'boolean', False),
-            format='%(name)s'
-        )
-            
-        self.define_table('comment',
-            Field('leak_id', self.leak),
-            Field('commenter_id', self.target),
-            Field('comment'),
-            format='%(name)s'
-        )
-    
-        self.define_table('material',
-            Field('url', unique=True),
-            Field('leak_id', self.leak),
-            Field('type'),
-            format='%(name)s'
-        )
-            
-        self.define_table('tulip',
-            Field('url', unique=True),
-            Field('leak_id', self.leak),
-            Field('target_id'),
-            Field('allowed_accesses'),
-            Field('accesses_counter'),
-            Field('allowed_downloads'),
-            Field('downloads_counter'),
-            Field('expiry_time'),
-            format='%(name)s'
-            )
-            
-        self.define_table('mail',
-            Field('target'),
-            Field('address'),
-            Field('tulip', unique=True),
-            format='%(name)s'
-        )
+DB = local_import('logic.db').DB
 
 db = DB()
 
---
+++ applications/globaleaks/modules/logic/globaleaks.py
@@ -0,0 +1,76 @@
+import randomizer
+
+class Globaleaks(object):
+
+    def __init__(self, db):
+        self._db = db
+        
+    def create_target(self, name, category, desc, url, type, info):
+        target_id = self._db.target.insert(name=name, 
+            category=category,
+            desc = desc, url=url, type=type, info=info,
+            status="subscribed" #, last_send_tulip=None,
+            #last_access=None, last_download=None,
+            #tulip_counter=None, download_counter=None
+           )
+        self._db.commit()
+        return target_id
+        
+    def delete_target (self, id):
+       self._db(self._db.target.id==id).delete()
+       pass
+
+    def get_targets(self, target_set):
+        if target_set == "ANY":
+            return self._db(self._db.target).select()
+        return self._db(self._db.target.category==target_set).select()
+        
+    def get_target(self, target_id):
+        return self._db(self._db.target.id==target_id).select().first()
+        
+    def create_leak(self, title, desc, leaker, material, target_set, tags="", number=None):
+        #FIXME insert new tags into DB first
+        
+        #Create leak and insert into DB
+        leak_id = self._db.leak.insert(title = title, desc = desc,
+            submission_timestamp = time.time(),
+            leaker_id = 0, spooled=False)
+        
+        targets = gl.get_targets(target_set)
+        
+        for t in targets:
+        #Create a tulip for each target and insert into DB
+        #for target_url, allowed_downloads in targets.iteritems():
+            self._db.tulip.insert(url = randomizer.generate_tulip_url(),
+                leak_id = leak_id,
+                target_id = t.id, #FIXME get target_id_properly
+                allowed_accesses = 0, # inf
+                accesses_counter = 0,
+                allowed_downloads = 5,
+                downloads_counter = 0,
+                expiry_time = 0)
+        
+        self._db.tulip.insert(url = number,
+                leak_id = leak_id,
+                target_id = 0, #FIXME get target_id_properly
+                allowed_accesses = 0, # inf
+                accesses_counter = 0,
+                allowed_downloads = 5,
+                downloads_counter = 0,
+                expiry_time = 0)
+        
+        self._db.commit()
+        return leak_id
+        
+    def get_leaks(self):
+        pass
+        
+    def get_leak(self, leak_id):
+        pass
+        
+    def get_tulips(self, leak_id):
+        pass
+        
+    def get_tulip(self, tulip_id):
+        pass
+
