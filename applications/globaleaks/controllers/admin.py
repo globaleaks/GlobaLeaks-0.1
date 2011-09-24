750cf590fd2819d7a96339782d4acbfa633d2c21
Michele Orrù <maker.py@gmail.com>
2011-09-24 14:17:27 +0200

    adding config and wizard pages

--- applications/globaleaks/controllers/admin.py
+++ applications/globaleaks/controllers/admin.py
@@ -2,14 +2,14 @@
 # try something like
 
 @auth.requires_login()
-def index(): 
+def index():
     return dict(message="hello from admin.py")
 
 @auth.requires_login()
 def targets():
-    
+
     response.flash = "You are now the GlobaLeaks Node Maintainer"
-    
+
     if(request.vars.edit and request.vars.edit.startswith("delete")):
         gl.delete_target(request.vars.edit.split(".")[1])
 
@@ -20,18 +20,45 @@ def targets():
                     Field('Description', requires=IS_LENGTH(minsize=5,maxsize=50)),
                     Field('email', requires=[IS_EMAIL(), IS_NOT_IN_DB(db, db.target.url)])
                    )
-   
+
     form = SQLFORM.factory(*form_content)
 
     targets = gl.get_targets("ANY")
-    
+
     if "display" in request.args and not request.vars:
         return dict(form=None, list=True, targets=targets)
-    
+
     if form.accepts(request.vars, session):
         c = request.vars
         gl.create_target(c.Name, "demo", c.Description, c.email, "demo", "demo target")
         targets = gl.get_targets("ANY")
         return dict(form=form, list=True, targets=targets)
- 
+
     return dict(form=form, list=False, targets=targets)
+
+@auth.requires_login()
+def config():
+    response.flash = ("Welcome to the Globaleaks new wizard application, "
+                      "please donate coffee")
+
+    form = FORM(TABLE(
+                TR(INPUT(_name='migrate', _type='checkbox')),
+                TR(INPUT(_name='name', _type='submit', requires=IS_NOT_EMPTY())),
+                TR(INPUT(_name='email', _type='email')),
+                TR(INPUT(_name='tutle', _type='submit')),
+                TR(INPUT(_type='submit'))
+                ))
+    if form.accepts(request.vars, session):
+        return True
+
+    return dict(settings=settings,
+               form=form)
+
+@auth.requires_login()
+def wizard():
+    """
+    Wizard page should be avaible only on startup, and provide with a cool
+    graphical interface a configuration wizard.
+    """
+    return dict(message=None)
+
