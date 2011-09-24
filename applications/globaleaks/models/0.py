0fa47089641efbee9b88339c23318d532d4891f1
Michele Orrù <maker.py@gmail.com>
2011-09-24 13:40:07 +0200

    creating config file

--- applications/globaleaks/models/0.py
+++ applications/globaleaks/models/0.py
@@ -4,41 +4,13 @@ import os.path
 from gluon.storage import Storage
 from gluon.tools import Mail, Auth
 
+from config import ConfigFile
+
 db = local_import('logic.db').DB()
 gl = local_import('logic.globaleaks').Globaleaks(db)
 
 mail = Mail(db)
 auth = Auth(db)
-#
-#############################
-#beginning of another file
-class ConfigFile(Storage):
-
-    def __init__(self, cfgparser, section):
-        super(ConfigFile, self).__init__()
-
-        self._cfgparser = cfgparser
-        self._section = section
-
-    def __getattr__(self, name):
-        if name.startswith('_'):
-            return self.__dict__.get(name, None)
-        try:
-            return self._cfgparser.get(self._section, name)
-        except ConfigParser.NoOptionError:
-            raise NameError
-
-    def __setattr__(self, name, value):
-        # keep an open port with private attributes
-        if name.startswith('_'):
-            self.__dict__[name] = value
-        try:
-            self._cfgparser.set(self._section, name, value)
-        except ConfigParser.NoOptionError:
-            raise NameError
-#
-# end of another file
-##############################
 
 cfgparser = ConfigParser.ConfigParser()
 cfgfile = os.path.join(os.path.dirname(__file__), 'gleaks.cfg')
