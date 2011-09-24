4da5eaf715656dec31d8f2abb2876b79d9c92a6c
vecna <vecna@delirandom.net>
2011-09-24 22:31:21 +0200

    perm in crontab, routes

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ default_function    = 'index'
 
 routes_in = (
              ('^/tulip(/)?', '/globaleaks/tulip/index'),
-             ('^/target(/)?', '/globaleaks/admin/targets'),
+             ('^/targets(/)?', '/globaleaks/admin/targets'),
              ('^/tulip/(?P<tulip_id>[\w]+)', '/globaleaks/tulip/status/\g<tulip_id>'),
              ('^/submit(/)?(?P<any>.*)', '/globaleaks/submission/\g<any>'),
             )
