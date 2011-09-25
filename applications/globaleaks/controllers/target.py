e1ffe8a1903319713508406ded2eac698cb44a7e
volpino <fox91@anche.no>
2011-09-25 05:30:48 +0200

    setup.py! now it's really simple to deploy :D

--- applications/globaleaks/controllers/target.py
+++ applications/globaleaks/controllers/target.py
@@ -9,25 +9,25 @@ def subscribe():
                                 )
         if subscribe_form.accepts(request.vars, session):
             return dict(message="Not implemented!",subscribe=None)
-            
+
         return dict(message="Here you can subscribe as a target",subscribe=subscribe_form)
-        
+
     if request.args:
         tulip_url = request.args[0]
     else:
         tulip_url = None
-            
+
     try:
         tulip = Tulip(url=tulip_url)
     except:
         return dict(message="Error!",subscribe=None)
-    
+
     if not tulip_url or tulip.target == "0":
         return dict(message="Error!",subscribe=None)
-        
+
     else:
         target = db(db.target.id==tulip.target).select().first()
-        
+
         if not target:
             return dict(message="Error!",subscribe=None)
 
@@ -37,7 +37,7 @@ def subscribe():
         else:
             db.target[tulip.target].update_record(status="subscribed")
             return dict(message="subscribed",subscribe=None)
-            
+
     return dict(message="this is logically impossible",subscribe=None)
 
 def unsubscribe():
@@ -45,26 +45,26 @@ def unsubscribe():
         tulip_url = request.args[0]
     else:
         tulip_url = None
-            
+
     try:
         tulip = Tulip(url=tulip_url)
     except:
         return dict(message="Error!")
-    
+
     if not tulip_url or tulip.target == "0":
         return dict(message="Error!")
-        
+
     else:
         target = db(db.target.id==tulip.target).select().first()
-        
+
         if not target:
             return dict(message="Error!")
-        
+
         if target.status == "unsubscribed":
             return dict(message="already unsubscribed")
 
         else:
             db.target[tulip.target].update_record(status="unsubscribed")
             return dict(message="unsubscribed")
-            
+
     return dict(message="this is logically impossible")
--- setup.py
+++ setup.py
@@ -1,18 +1,59 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from glob import glob
+from setuptools import setup, find_packages
 
+import os, glob, fnmatch
 
-packages = ['applications',
-            '']
+def opj(*args):
+    path = os.path.join(*args)
+    return os.path.normpath(path)
 
+def find_data_files(srcdir, *wildcards, **kw):
+    # get a list of all files under the srcdir matching wildcards,
+    # returned in a format to be used for install_data
+    def walk_helper(arg, dirname, files):
+        if '.svn' in dirname:
+            return
+        names = []
+        lst, wildcards = arg
+        for wc in wildcards:
+            wc_name = opj(dirname, wc)
+            for f in files:
+                filename = opj(dirname, f)
+
+                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
+                    names.append(filename)
+        if names:
+            lst.append( (dirname, names ) )
+
+    file_list = []
+    recursive = kw.get('recursive', True)
+    if recursive:
+        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
+    else:
+        walk_helper((file_list, wildcards),
+                    srcdir,
+                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
+    return file_list
+
+data_files = [('globaleaks',['VERSION'])]
+for elem in find_data_files('applications/', '*.*'):
+    data_files.append(("globaleaks/"+elem[0], elem[1]))
+
+#packages = ['globaleaks.'+x for x in find_packages()]+['globaleaks']
+packages = ['globaleaks']
+#package_data = find_package_data(where="applications",
+#               package="globaleaks.applications")
 setup(name='Globaleaks',
       version='0.hackathon',
       description='A flexible Opensource Whistleblowing Platform',
       author='Random Globaleaks Developers',
+      author_email='info@globaleaks.org',
       url='http://www.globaleaks.org/',
-      requires=['gluon']
+      requires=['web2py'],
+      package_dir={'globaleaks': ''},
       packages=packages,
-
+      #package_data=package_data,
+      data_files=data_files,
+      scripts=['startglobaleaks'],
      )
