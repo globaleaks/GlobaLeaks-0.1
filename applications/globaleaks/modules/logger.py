49a543940bdb38c98f30c6f54296fdc1b7eaf622
Michele Orrù <maker.py@gmail.com>
2011-09-29 23:19:10 +0200

    logging options

--- applications/globaleaks/modules/logger.py
+++ applications/globaleaks/modules/logger.py
@@ -7,5 +7,8 @@ def get_logger(settings):
         formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
         hdlr.setFormatter(formatter)
         logger.addHandler(hdlr)
+        if settings.logging.server and settings.logging.client:
+            logger.setLevel(logging.ERROR)
+        else:
         logger.setLevel(logging.INFO)
     return logger
