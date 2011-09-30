1973b5c15f062a06f9375ef8d7f57492219b579c
volpino <fox91@anche.no>
2011-09-30 09:50:39 +0200

    Moving everything to globaleaks directory for setup.py fix + targetgroups view fix

--- setup.py
+++ setup.py
@@ -1,45 +1,12 @@
 #!/usr/bin/env python
 
-from setuptools import setup
-
-import os, glob, fnmatch
-
-from applications import globaleaks
-def opj(*args):
-    path = os.path.join(*args)
-    return os.path.normpath(path)
-
-def find_data_files(srcdir, *wildcards, **kw):
-    # get a list of all files under the srcdir matching wildcards,
-    # returned in a format to be used for install_data
-    def walk_helper(arg, dirname, files):
-        if '.git' in dirname:
-            return
-        names = []
-        lst, wildcards = arg
-        for wc in wildcards:
-            wc_name = opj(dirname, wc)
-            for f in files:
-                filename = opj(dirname, f)
-
-                if fnmatch.fnmatch(filename, wc_name) and not os.path.isdir(filename):
-                    names.append(filename)
-        if names:
-            lst.append( (dirname, names ) )
-
-    file_list = []
-    recursive = kw.get('recursive', True)
-    if recursive:
-        os.path.walk(srcdir, walk_helper, (file_list, wildcards))
-    else:
-        walk_helper((file_list, wildcards),
-                    srcdir,
-                    [os.path.basename(f) for f in glob.glob(opj(srcdir, '*'))])
-    return file_list
-
-data_files = [('globaleaks',['VERSION'])]
-for elem in find_data_files('applications', '*.*'):
-    data_files.append((os.path.join("globaleaks", elem[0]), elem[1]))
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+import globaleaks
 
 packages = ['globaleaks']
 
@@ -49,9 +16,12 @@ setup(name=globaleaks.__name__,
       author=globaleaks.__authors__,
       author_email=globaleaks.__mail__,
       url=globaleaks.__site__,
-      install_requires=['web2py'],
-      package_dir={'globaleaks': ''},
-      packages=packages,
-      data_files=data_files,
-      scripts=['startglobaleaks'],
+      #install_requires=['web2py'],
+      packages=find_packages(),
+      zip_safe=False,
+      extras_require = {},
+      entry_points="""
+      """,
+      include_package_data=True,
+      scripts=['globaleaks/startglobaleaks'],
      )
