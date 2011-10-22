9cc171640a31e7be9abc9a5a5728e2321e6bd11a
Michele Orrù <maker.py@gmail.com>
2011-10-22 11:36:38 +0200

    Issue #51: implementing zip encryption.

--- globaleaks/applications/globaleaks/modules/compress_material.py
+++ globaleaks/applications/globaleaks/modules/compress_material.py
@@ -1,11 +1,14 @@
 import zipfile
-import os
+import os.path
+import subprocess
+
+from os import listdir
 
 class Zip:
     """
     Class that creates the material archive.
     """
-    def create_zip(self, db, submission, request, logger):
+    def create_zip(self, db, submission, request, logger, passwd=None):
         """
         Function to create an unencrypted zipfile
         """
@@ -14,27 +17,35 @@ class Zip:
                 filedir = str(db(db.submission.leak_id==submission.id).select(
                           db.submission.dirname).first().dirname)
             except:
-                logger.error("create_zip: invalid filedir")
+                logger.error('create_zip: invalid filedir')
                 return dict(error='invalid filedir')
 
             try:
                 mat_dir = os.path.join(request.folder, 'material/') + filedir
-                logger.info("mat_dir %s\n", mat_dir)
-                logger.info("path %s\n",
-                            os.path.join(mat_dir, filedir + ".zip"))
-                zipf = zipfile.ZipFile(mat_dir+".zip", 'w')
-                if mat_dir:
-                    for f in os.listdir(mat_dir):
-                        zipf.write(mat_dir+"/"+f, f)
-            except:
-                logger.error("create_zip: error in creating zip")
+                logger.info('mat_dir %s', mat_dir)
+                logger.info('path %s',
+                            os.path.join(mat_dir, filedir + '.zip'))
+                # XXX: issue #51
+                if passwd and os.path.exists(mat_dir):
+                    cmd = 'zip -e -P%(passwd) %(zipfile).zip %(files)' % dict(
+                           passwd = passwd, zipfile=matdir,
+                           files = ' '.join(listdir(mat_dir)))
+                    subprocess.check_call(cmd.split())
+                elif not passwd and os.path.exists(mat_dir):
+                    zipf = zipfile.ZipFile(mat_dir+'.zip', 'w')
+                    for f in listdir(mat_dir):
+                        zipf.write(os.path.join(mat_dir, f), f)
+                else:
+                    logger.error('create_zip: invalid path')
+            except RuntimeError as err:
+                logger.error('create_zip: error in creating zip')
                 try:
                     zipf.close()
-                except:
-                    pass
-                return dict(error='error creating zip')
-
-            zipf.close()
-
-    def create_encrypted_zip():
-        pass
+                except (RuntimeError, zipfile.error) as err:
+                    logger.info('create_zip: error when trying to save zip')
+            except subprocess.CalledProcessError as err :
+                    logger.error('create_zip: error in creating zip')
+            else:
+                err = None
+            finally:
+                return dict(error=err) if err else None
