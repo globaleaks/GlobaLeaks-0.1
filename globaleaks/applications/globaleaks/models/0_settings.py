36c1df4e522782875eab03e90cd103bae79016f9
Giovanni Pellerano <evilaliv3@remotehost.(none)>
2011-10-30 17:19:34 +0100

    working with the elephant

--- globaleaks/applications/globaleaks/models/0_settings.py
+++ globaleaks/applications/globaleaks/models/0_settings.py
@@ -4,16 +4,27 @@ import os.path
 from gluon.storage import Storage
 from config import ConfigFile, cfgfile
 
+class ThreadSafe(dict):
+    forever=10**10
+    def __init__(self):
+         import thread
+         self['lock']=thread.allocate_lock()
+    def __setattr__(self,key,value):
+         self['lock'].acquire()
+         self[key]=value
+         self['lock'].release()
+    def __getattr__(self,key):
+         self['lock'].acquire()
+         value=self[key]
+         self['lock'].release()
+         return value
+
+x=cache.ram('elefante_in_sala_server',lambda:ThreadSafe(),ThreadSafe.forever)
+
+
 ################################################################
 # Import the database and global settings from the config file #
 ################################################################
-
 settings = Storage()
-
 settings.globals = ConfigFile(cfgfile, 'global')
 settings.database = ConfigFile(cfgfile, 'database')
-
-
-#######
-# XXX: SHIT HAPPENS
-######
