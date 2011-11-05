0eab3a6a8c8e02c1766433e64ea91a70cad80d5a
Arturo Filastò <hellais@gmail.com>
2011-11-05 12:57:26 +0100

    Make pretty colors

--- globaleaks/installstorage/post_install_routes.py
+++ globaleaks/installstorage/post_install_routes.py
@@ -2,19 +2,22 @@ default_application = 'globaleaks'
 default_controller  = 'default'
 default_function    = 'index'
 
-routes_in = (
-             ('^/nodeprivacy(/)?', '/globaleaks/admin/nodeprivacy'),
-             ('^/tulip(/)?', '/globaleaks/tulip/index'),
-             ('^/targets(/)?', '/globaleaks/admin/targets'),
-             ('^/tulip/(?P<tulip_id>[\w]+)', '/globaleaks/tulip/status/\g<tulip_id>'),
-             ('^/submit(/)?(?P<any>.*)', '/globaleaks/submission/\g<any>'),
-             ('^/subscribe(/)?', '/globaleaks/target/subscribe/'),
-             ('^/unsubscribe(/)?', '/globaleaks/target/unsubscribe/'),
-             ('^/groups(/)?', '/globaleaks/admin/targetgroups'),
-             ('^/globalview(/)?', '/globaleaks/target/view'),
-             ('^/receiver(/)?', '/globaleaks/target/receiver'),
-             ('^/bouquet/(?P<target_id>[\w]+)', '/globaleaks/target/bouquet/\g<target_id>'),
-             ('^/bouquet', '/globaleaks/target/bouquet'),
-             ('^/js', '/globaleaks/preload/js'),
-             ('^/css', '/globaleaks/preload/css')
-            )
+if 1:
+    routes_in = (
+                 ('^/nodeprivacy(/)?', '/globaleaks/admin/nodeprivacy'),
+                 ('^/tulip(/)?', '/globaleaks/tulip/index'),
+                 ('^/targets(/)?', '/globaleaks/admin/targets'),
+                 ('^/tulip/(?P<tulip_id>[\w]+)', '/globaleaks/tulip/status/\g<tulip_id>'),
+                 ('^/submit(/)?(?P<any>.*)', '/globaleaks/submission/\g<any>'),
+                 ('^/subscribe(/)?', '/globaleaks/target/subscribe/'),
+                 ('^/unsubscribe(/)?', '/globaleaks/target/unsubscribe/'),
+                 ('^/groups(/)?', '/globaleaks/admin/targetgroups'),
+                 ('^/globalview(/)?', '/globaleaks/target/view'),
+                 ('^/receiver(/)?', '/globaleaks/target/receiver'),
+                 ('^/bouquet/(?P<target_id>[\w]+)', '/globaleaks/target/bouquet/\g<target_id>'),
+                 ('^/bouquet', '/globaleaks/target/bouquet'),
+                 ('^/js', '/globaleaks/preload/js'),
+                 ('^/css', '/globaleaks/preload/css')
+                )
+
+
