482fc1e7562957be4122a93267a3ea6542625210
Arturo Filastò <hellais@gmail.com>
2011-11-06 01:03:46 +0100

    use gluon copystream function

--- globaleaks/applications/globaleaks/controllers/upload_test.py
+++ globaleaks/applications/globaleaks/controllers/upload_test.py
@@ -1,4 +1,6 @@
 import gluon.contrib.simplejson as json
+from gluon.fileutils import copystream
+
 import os
 import shutil
 
@@ -6,5 +8,7 @@ def index():
     return {}
 
 def post():
-    shutil.copyfileobj(request.body, open("/dev/null", "wb"))
+    # shutil.copyfileobj(request.body, open("/dev/null", "wb"))
+    print request.env.content_length
+    copystream(request.body, open('/dev/null', "wb"), int(request.env.content_length))
     return json.dumps({})
