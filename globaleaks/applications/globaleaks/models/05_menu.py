3747ee8b522d5a481fd8a49de8f87557f3bc3c89
Fabio Pietrosanti <fabio@infosecurity.ch>
2011-11-20 17:03:48 +0100

    Modified Menu: Disabled Groups, receiver, added Target Preferences, Admin Config, Admin Logout

--- globaleaks/applications/globaleaks/models/05_menu.py
+++ globaleaks/applications/globaleaks/models/05_menu.py
@@ -5,28 +5,30 @@ response.meta.keywords = settings.keywords
 response.meta.description = settings.description
 
 
+# Is this menu required?
 response.menu = [
     (T('Index'),URL('default','index')==URL(),URL('default','index'),[]),
     (T('Submission'),URL('submission', 'index')==URL(),URL('submission','index'),[]),
-    (T('Receiver'), '/globaleaks/target/receiver'==URL(), '/globaleaks/target/receiver', []),
-    (T('Node View'), '/globaleaks/target/view'==URL(), '/globaleaks/target/view', []),
-    (T('Targets'),'/globaleaks/admin/targets'==URL(), '/globaleaks/admin/targets'),
-    (T('Groups'),'/globaleaks/admin/targetgroups'==URL(),'/globaleaks/admin/targetgroups'),
-    # (T('Tulips'),URL('tulip','index')==URL(),URL('tulip','index'),[]),
-    # "Tulips" redirect to index, why keeping that ?
+#    (T('Receiver'), '/globaleaks/target/receiver'==URL(), '/globaleaks/target/receiver', []),
+#    (T('Node View'), '/globaleaks/target/view'==URL(), '/globaleaks/target/view', []),
+#    (T('Targets'),'/globaleaks/admin/targets'==URL(), '/globaleaks/admin/targets'),
+#    (T('Groups'),'/globaleaks/admin/targetgroups'==URL(),'/globaleaks/admin/targetgroups'),
+#    (T('Tulips'),URL('tulip','index')==URL(),URL('tulip','index'),[]),
+#    # "Tulips" redirect to index, why keeping that ?
 ]
 
 response.menu_target = [
     (T('Tulip'),'/globaleaks/tulip/status/'+str(session.target)==URL(), '/globaleaks/tulip/status/'+str(session.target)),
-    #(T('Preferences'),'/preferences'==URL(),'/preferences'),
+#   (T('Preferences'),'/preferences'==URL(),'/preferences'),
     (T('Bouquet'),'/globaleaks/target/bouquet/'==URL(), '/globaleaks/target/bouquet/'),
+    (T('Preferences'),'/globaleaks/target/preferences/'==URL(), '/globaleaks/target/preferences/'),
 ]
 
 response.menu_admin = [
     (T('Targets'),'/globaleaks/admin/targets/display'==URL(), '/globaleaks/admin/targets/display'),
-    (T('Groups'),'/globaleaks/admin/targetgroups'==URL(),'/globaleaks/admin/targetgroups'),
-    (T('Globalview'),'/globaleaks/target/view'==URL(), '/globaleaks/target/view'),
-    (T('Config'),'/config'==URL(), '/config'),
-    (T('Stats'),'/stats'==URL(), '/stats'),
+#    (T('Groups'),'/globaleaks/admin/targetgroups'==URL(),'/globaleaks/admin/targetgroups'),
+#    (T('Globalview'),'/globaleaks/target/view'==URL(), '/globaleaks/target/view'),
+    (T('Config'),'/globaleaks/admin/config'==URL(), '/globaleaks/admin/config'),
     (T('Logout'),'/globaleaks/default/user/logout'==URL(), '/globaleaks/default/user/logout'),
+    (T('Password'),'/globaleaks/default/user/change_password'==URL(), '/globaleaks/default/user/change_password'),
 ]
