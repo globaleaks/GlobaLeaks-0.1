74e4147534a1ad3c02b048fb30ffbc1941b7260e
vecna <vecna@delirandom.net>
2011-11-20 17:09:55 +0100

    fixed wrong and misleading naming. targets => receivers

--- globaleaks/applications/globaleaks/models/05_menu.py
+++ globaleaks/applications/globaleaks/models/05_menu.py
@@ -23,7 +23,7 @@ response.menu_target = [
 ]
 
 response.menu_admin = [
-    (T('Targets'),'/globaleaks/admin/targets/display'==URL(), '/globaleaks/admin/targets/display'),
+    (T('Receivers'),'/globaleaks/admin/targets/display'==URL(), '/globaleaks/admin/targets/display'),
     (T('Groups'),'/globaleaks/admin/targetgroups'==URL(),'/globaleaks/admin/targetgroups'),
     (T('Globalview'),'/globaleaks/target/view'==URL(), '/globaleaks/target/view'),
     (T('Config'),'/config'==URL(), '/config'),
